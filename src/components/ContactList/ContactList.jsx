cc7844971a9b95cc2142e2d48376db18f2cd2e56
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-04-21 21:17:40 +0300

    shorten the reducer code

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,34 +1,42 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchContacts } from "redux/operations";
+import { fetchContacts } from 'redux/operations';
 import { ContactItem } from 'components/ContactItem/ContactItem';
-import { getContacts, getFilter } from 'redux/selectors';
+import {
+  getContacts,
+  getFilter,
+  getError,
+  getIsLoading,
+} from 'redux/selectors';
 
 import { ListContacts } from './ContactList.styled';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
-  const {items, isLoading, error } = useSelector(getContacts);
+  const { items } = useSelector(getContacts);
   const filter = useSelector(getFilter);
-  
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
   useEffect(() => {
     dispatch(fetchContacts());
-   
   }, [dispatch]);
 
   const visibleContacts = items.filter(contacts =>
     contacts.name.toLowerCase().includes(filter.toLowerCase())
   );
 
-
   return (
     <ListContacts>
-      {isLoading && !error  && <p>Loading Contacts...</p>}
+      {isLoading && !error && <p>Loading Contacts...</p>}
       {error && <p>{error}</p>}
-      {visibleContacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {!isLoading &&
+        !error &&
+        visibleContacts.map(contact => (
+          <ContactItem key={contact.id} contact={contact} />
+        ))}
     </ListContacts>
   );
 };
+
 
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,39 +4,36 @@ import {
 } from '@reduxjs/toolkit';
 import { fetchContacts, addContacts, deleteContacts } from './operations';
 
+const handlePending = state => {
+  state.isLoading = true;
+};
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: { items: [], isLoading: false, error: null },
 
   extraReducers: {
-    [fetchContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchContacts.pending]: handlePending,
     [fetchContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items = action.payload;
     },
-    [fetchContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [addContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchContacts.rejected]: handleRejected,
+    
+    [addContacts.pending]: handlePending,
     [addContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items.push(action.payload);
     },
-    [addContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+    [addContacts.rejected]: handleRejected,
 
-    [deleteContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [deleteContacts.pending]: handlePending,
     [deleteContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
@@ -45,10 +42,7 @@ const contactsSlice = createSlice({
       );
       state.items.splice(index, 1);
     },
-    [deleteContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+    [deleteContacts.rejected]: handleRejected,
   },
 });
 
