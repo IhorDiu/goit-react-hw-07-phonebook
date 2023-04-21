7e2990a86ebd0077d02b764f6b7ec059a8b0dee8
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-04-21 22:52:41 +0300

    converted extraReducers to functional code

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,7 +1,4 @@
-import {
-  createSlice,
-  //  nanoid
-} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, addContacts, deleteContacts } from './operations';
 
 const handlePending = state => {
@@ -16,34 +13,59 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState: { items: [], isLoading: false, error: null },
 
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [fetchContacts.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [fetchContacts.rejected]: handleRejected,
-    
-    [addContacts.pending]: handlePending,
-    [addContacts.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(action.payload);
-    },
-    [addContacts.rejected]: handleRejected,
+  extraReducers: builder =>
+    builder
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContacts.pending, handlePending)
+      .addCase(addContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(addContacts.rejected, handleRejected)
+      .addCase(deleteContacts.pending, handlePending)
+      .addCase(deleteContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const index = state.items.findIndex(
+          contact => contact.id === action.payload.id
+        );
+        state.items.splice(index, 1);
+      })
+      .addCase(deleteContacts.rejected, handleRejected),
+  //   [fetchContacts.pending]: handlePending,
+  //   [fetchContacts.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     state.items = action.payload;
+  //   },
+  //   [fetchContacts.rejected]: handleRejected,
 
-    [deleteContacts.pending]: handlePending,
-    [deleteContacts.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(
-        contact => contact.id === action.payload.id
-      );
-      state.items.splice(index, 1);
-    },
-    [deleteContacts.rejected]: handleRejected,
-  },
+  //   [addContacts.pending]: handlePending,
+  //   [addContacts.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     state.items.push(action.payload);
+  //   },
+  //   [addContacts.rejected]: handleRejected,
+
+  //   [deleteContacts.pending]: handlePending,
+  //   [deleteContacts.fulfilled](state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     const index = state.items.findIndex(
+  //       contact => contact.id === action.payload.id
+  //     );
+  //     state.items.splice(index, 1);
+  //   },
+  //   [deleteContacts.rejected]: handleRejected,
+  // },
 });
 
 export const contactsReducer = contactsSlice.reducer;
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,42 +3,38 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://643e9914c72fda4a0bfb6ad1.mockapi.io';
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', 
-  // Використовуємо символ підкреслення як ім'я першого параметра,
-  // тому що в цій операції він нам не потрібен
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/contacts");
-      // При успішному запиті повертаємо проміс із даними
+      const response = await axios.get('/contacts');
       return response.data;
     } catch (e) {
-      // При помилці запиту повертаємо проміс
-      // який буде відхилений з текстом помилки
       return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
 
 export const addContacts = createAsyncThunk(
-    "contacts/addContacts",
-    async (values, thunkAPI) => {
-      try {
-        const response = await axios.post("/contacts", values);
-        return response.data;
-      } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-      }
+  'contacts/addContacts',
+  async (values, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', values);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
-  );
+  }
+);
 
-  export const deleteContacts = createAsyncThunk(
-    "contacts/deleteContacts",
-    async (id, thunkAPI) => {
-      try {
-        const response = await axios.delete(`/contacts/${id}`);
-        return response.data;
-      } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-      }
+export const deleteContacts = createAsyncThunk(
+  'contacts/deleteContacts',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${id}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
-  );
+  }
+);
